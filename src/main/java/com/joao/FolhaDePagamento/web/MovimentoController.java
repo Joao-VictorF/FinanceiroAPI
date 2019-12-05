a36d575223009305e70e2bfb268d73f1ff374912
Juaozim <freitasjoaovictor49@gmail.com>
2019-12-05 19:26:21 -0300

    Adicionando o Cors nos controllers e erro no heroku corrigido

--- src/main/java/com/joao/FolhaDePagamento/web/MovimentoController.java
+++ src/main/java/com/joao/FolhaDePagamento/web/MovimentoController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,8 @@ import com.joao.FolhaDePagamento.domain.Folha_Pagamento;
 import com.joao.FolhaDePagamento.domain.Movimento;
 import com.joao.FolhaDePagamento.domain.MovimentoRepository;
 import com.joao.FolhaDePagamento.domain.MovimentoType;
+
+@CrossOrigin
 @RestController
 public class MovimentoController {
 	@Autowired
